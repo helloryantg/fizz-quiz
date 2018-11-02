6e789aadf920c2be9b24df929207c7edbb79315d
Elizabeth Messick <Elizabeth@MacBook-Pro-3.local>
2018-11-02 14:01:16 -0700

    add database connection and .env file

--- config/database.js
+++ config/database.js
@@ -1,1 +1,9 @@
-asd
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+mongoose.connection.once('open', function () {
+    console.log(`Mongoose connect to: ${process.env.DATABASE_URL}`);
+});
+
+module.exports = mongoose;
